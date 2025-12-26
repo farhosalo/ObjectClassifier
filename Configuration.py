22864393eeed8b2ba0a9d89525f11d84bb0fa10b
Salo <info@salo.com.de>
2025-12-26 14:18:39 +0100

    Add configuration file

---
+++ Configuration.py
@@ -0,0 +1,17 @@
+config = {
+    "app": {
+        "CLASS_NAME_FILE": "ClassName.txt",
+        "MODEL_FILE": "ObjectClassifier.keras",
+        "MODEL_DIR": "./",
+    },
+    "dataset": {
+        "DATASET_DIR": "Data",
+        "IMAGE_HEIGHT": 224,
+        "IMAGE_WIDTH": 224,
+        "DIR": "Signs",
+        "BATCH_SIZE": 10,
+    },
+    "model": {
+        "EPOCHS": 30,
+    },
+}
